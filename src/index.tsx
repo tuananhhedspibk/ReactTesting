bb4d7f2599bb40308d2abc0f3aa2cca8ba2ffde0
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2025-02-09 10:47:56 +0900

    :truck: Re-struct folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import App from './components/App';
+import './styles/index.css';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
